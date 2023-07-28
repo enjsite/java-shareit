adfac1c2b1a9cc62ab02eb64c96c76451616253e
enjsite <enjyweb@gmail.com>
2023-07-28 10:10:46 +0300

    feature: TZ15 add-item-request, ItemRepository tests

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -3,12 +3,13 @@ package ru.practicum.shareit.item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 import java.util.Optional;
 
-@Component
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(" select i from Item i " +
--- src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemRepositoryTest.java
@@ -2,10 +2,16 @@ package ru.practicum.shareit.item;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class ItemRepositoryTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
 
     @BeforeEach
     void setUp() {
